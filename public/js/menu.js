e1c8c3b82d75ed29f575c72369aaf47d3dbefd82
Samuel Barnholdt <samuel.barnholdt@gmail.com>
2019-02-03 15:33:41 +0100

    work

---
+++ public/js/menu.js
@@ -0,0 +1,29 @@
+var bigkahuna = [
+    {
+        "name": "Big Kahuna Burger",
+        "kCal": 850,
+        "lactose": true,
+        "gluten": true,
+        "img": "img/bigkahuna.jpg"
+    }
+]
+
+var animal = [
+    {
+        "name": "Animal Style ",
+        "kCal": 850,
+        "lactose": true,
+        "gluten": true,
+        "img": "img/innout.jpg"
+    }
+]
+
+var royale = [
+    {
+        "name": "Royale With Cheese",
+        "kCal": 850,
+        "lactose": true,
+        "gluten": true,
+        "img": "img/royalewithcheese.jpg"
+    }
+]
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,15 +1,24 @@
-var menuitem = new Vue({
-    el: '#menuitems',
+
+
+Vue.component('burger',{
+    template: `<p>okey</p>`
+})
+
+var app = new Vue({
+    el: '#app',
     data: {
         bc: 'Big Kahuna Burger',
         item1:  ':(',
         item2:  ':(',
         item3:  ':(',
         burgers: [
-            { name: "bigkahuna", kcal: 500, chosen: false},
-            { name: "royale", kcal: 605, chosen: false},
-            { name: "animal", kcal: 650, chosen: false},
-        ]
+            { name: "Big Kahuna Burger", kcal: 500, chosen: false},
+            { name: "Royale With Cheese", kcal: 605, chosen: false},
+            { name: "Animal Style", kcal: 650, chosen: false},
+        ],
+        bk: bigkahuna,
+        as: animal,
+        roy: royale
 
     },
     methods: {
@@ -17,33 +26,58 @@ var menuitem = new Vue({
             this.item1 == ':)'?
                 this.item1 = ':(':
                 this.item1 = ':)'
-
+            this.select("Big Kahuna Burger")
         },
         pick2: function () {
             this.item2 == ':)'?
                 this.item2 = ':(':
                 this.item2 = ':)'
-            
+            this.select("Royale With Cheese");
         },
         pick3: function () {
             this.item3 == ':)'?
                 this.item3 = ':(':
                 this.item3 = ':)'
-
+            this.select("Animal Style");
         },
-        orders: function() {
-            var html = `<p>eymacarena</p>`;
-            return html;
-        }
+        select: function(name){
+            for (i = 0; i < this.burgers.length; i++){
+                if(this.burgers[i].name == name) this.burgers[i].chosen = !this.burgers[i].chosen;
+                else null
+            }
+        },
+
     }
-})
+});
 
 
-function listitem(){
-    var out = `<item></item>`
-    return out;
-}
+var bigkahuna = [
+    {
+        "name": "Big Kahuna Burger",
+        "kCal": 850,
+        "lactose": true,
+        "gluten": true,
+        "img": "img/bigkahuna.jpg"
+    }
+];
 
-Vue.component("item", {
-    template: `<li>` + 'Excellent Choice!' + `</li>`
-})
+var animal = [
+    {
+        "name": "Animal Style ",
+        "kCal": 850,
+        "lactose": true,
+        "gluten": true,
+        "img": "img/innout.jpg"
+    }
+];
+
+var royale = [
+    {
+        "name": "Royale With Cheese",
+        "kCal": 850,
+        "lactose": true,
+        "gluten": true,
+        "img": "img/royalewithcheese.jpg"
+    }
+];
+
